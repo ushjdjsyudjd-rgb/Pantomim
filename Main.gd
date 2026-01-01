2953dd73b7c7884cbb29b3bd87a5336c821f061f
ushjdjsyudjd-rgb <ushjdjsyudjd@gmail.com>
2026-01-01 23:55:34 +0330

    Update Main.gd

--- Main.gd
+++ Main.gd
@@ -1,55 +1,38 @@
 extends Control
 
-@onready var word_label = $WordLabel
-@onready var timer_label = $TimerLabel
-@onready var bg = $Background
+@onready var word_label = $MainPanel/WordLabel
+@onready var timer_label = $TopBar/TimerLabel
+@onready var progress_bar = $MainPanel/TimerProgress
 
 func _ready():
-    # اتصال دکمه‌ها
-    $CorrectButton.pressed.connect(_on_correct_pressed)
-    $SkipButton.pressed.connect(_on_skip_pressed)
-    
-    # تنظیم فونت
-    var f = load("res://assets/fonts/Vazir-Regular.ttf")
-    if f:
-        word_label.add_theme_font_override("font", f)
-        timer_label.add_theme_font_override("font", f)
-    
-    start_new_game()
+	setup_ui_style()
+	next_word()
 
-func start_new_game():
-    GameManager.time_left = 60
-    GameManager.score = 0
-    next_word()
-    
-    var timer = Timer.new()
-    add_child(timer)
-    timer.wait_time = 1.0
-    timer.timeout.connect(func():
-        if GameManager.time_left > 0:
-            GameManager.time_left -= 1
-            timer_label.text = str(GameManager.time_left)
-        else:
-            word_label.text = "وقت تمام! امتیاز: " + str(GameManager.score)
-            timer.stop()
-    )
-    timer.start()
+func setup_ui_style():
+	# ایجاد استایل بنفش نئونی شبیه عکس
+	var sb = StyleBoxFlat.new()
+	sb.bg_color = Color("#1e1e2e")
+	sb.set_border_width_all(2)
+	sb.border_color = Color("#ff00ff") # صورتی نئون
+	sb.corner_radius_top_left = 20
+	sb.corner_radius_bottom_right = 20
+	sb.shadow_size = 10
+	sb.shadow_color = Color(1, 0, 1, 0.2)
+	
+	$MainPanel.add_theme_stylebox_override("panel", sb)
 
 func next_word():
-    word_label.text = GameManager.get_random_word()
+	word_label.text = GameManager.get_random_word()
+	GameManager.time_left = 60
+	update_ui()
 
-func _on_correct_pressed():
-    if GameManager.time_left > 0:
-        GameManager.score += 1
-        flash_bg(Color.GREEN)
-        next_word()
+func _process(delta):
+	if GameManager.time_left > 0:
+		GameManager.time_left -= delta
+		update_ui()
+	elif GameManager.time_left <= 0:
+		word_label.text = "زمان تمام شد!"
 
-func _on_skip_pressed():
-    if GameManager.time_left > 0:
-        flash_bg(Color.RED)
-        next_word()
-
-func flash_bg(color):
-    var tw = create_tween()
-    tw.tween_property(bg, "color", color, 0.1)
-    tw.tween_property(bg, "color", Color("#1e1e2e"), 0.2)
+func update_ui():
+	timer_label.text = str(ceil(GameManager.time_left))
+	progress_bar.value = (GameManager.time_left / 60.0) * 100

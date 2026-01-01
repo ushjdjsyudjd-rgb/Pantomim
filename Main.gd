02402faf0e394c0a3a0c462dbb5f8b580035579e
ushjdjsyudjd-rgb <ushjdjsyudjd@gmail.com>
2026-01-02 00:13:27 +0330

    Update Main.gd

--- Main.gd
+++ Main.gd
@@ -1,58 +1,55 @@
 extends Control
 
-# ارجاع به گره‌های صحنه
 @onready var word_label = $MainPanel/WordLabel
 @onready var timer_label = $TopBar/TimerLabel
-@onready var score_label = $TopBar/ScoreLabel
 @onready var progress_bar = $MainPanel/TimerProgress
+@onready var score_label = $TopBar/ScoreLabel
 
 func _ready():
-	# تنظیم ظاهر نئونی در شروع بازی
-	apply_visual_style()
-	# شروع اولین دور بازی
-	next_round()
+    # تنظیمات ظاهری برای ابعاد ۱۲۸۰ در ۷۲۰
+    apply_visual_style()
+    next_round()
 
 func apply_visual_style():
-	# ایجاد استایل بنفش/صورتی نئون مشابه عکس
-	var style = StyleBoxFlat.new()
-	style.bg_color = Color("#1a1a2e") # سرمه‌ای تیره
-	style.border_width_left = 4
-	style.border_top_width = 4
-	style.border_right_width = 4
-	style.border_bottom_width = 4
-	style.border_color = Color("#ff00ff") # صورتی نئون
-	style.corner_radius_top_left = 25
-	style.corner_radius_bottom_right = 25
-	style.shadow_size = 20
-	style.shadow_color = Color(1, 0, 1, 0.2) # سایه ملایم نئونی
-	
-	$MainPanel.add_theme_stylebox_override("panel", style)
+    var style = StyleBoxFlat.new()
+    style.bg_color = Color("#1a1a2e")
+    style.border_width_all = 4
+    style.border_color = Color("#ff00ff")
+    style.corner_radius_top_left = 30
+    style.corner_radius_bottom_right = 30
+    style.shadow_size = 25
+    style.shadow_color = Color(1, 0, 1, 0.2)
+    
+    $MainPanel.add_theme_stylebox_override("panel", style)
 
 func _process(delta):
-	# مدیریت زمان در هر فریم
-	if GameManager.time_left > 0:
-		GameManager.time_left -= delta
-		update_ui()
-	else:
-		on_time_up()
+    if GameManager.time_left > 0:
+        GameManager.time_left -= delta
+        update_ui()
+    else:
+        on_time_up()
 
 func update_ui():
-	# به‌روزرسانی متن تایمر و امتیاز
-	timer_label.text = str(ceil(GameManager.time_left))
-	score_label.text = "امتیاز: " + str(GameManager.score)
-	
-	# به‌روزرسانی نوار پیشرفت (تایمر پر شونده)
-	progress_bar.value = (GameManager.time_left / 60.0) * 100
-	
-	# قرمز شدن تایمر در لحظات آخر
-	if GameManager.time_left < 10:
-		timer_label.add_theme_color_override("font_color", Color.RED)
-	else:
-		timer_label.add_theme_color_override("font_color", Color.WHITE)
+    timer_label.text = str(ceil(GameManager.time_left))
+    score_label.text = "امتیاز: " + str(GameManager.score)
+    progress_bar.value = (GameManager.time_left / 60.0) * 100
 
 func next_round():
-	# بازنشانی زمان و انتخاب کلمه جدید
-	GameManager.time_left = 60
-	word_label.text = GameManager.get_random_word()
-	
-	# انیمیشن ساده برای ظاهر شدن کلمه (Fade
+    GameManager.time_left = 60
+    word_label.text = GameManager.get_random_word()
+    # افکت انیمیشن برای کلمه جدید
+    var tween = create_tween()
+    word_label.scale = Vector2(0.8, 0.8)
+    tween.tween_property(word_label, "scale", Vector2(1.0, 1.0), 0.3).set_trans(Tween.TRANS_BACK)
+
+func on_time_up():
+    word_label.text = "وقت تموم شد!"
+    if OS.has_feature("mobile"):
+        Input.vibrate_handheld(500)
+
+func _on_correct_pressed():
+    GameManager.score += 10
+    next_round()
+
+func _on_wrong_pressed():
+    next_round()
